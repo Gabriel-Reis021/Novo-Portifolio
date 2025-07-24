79338827e1586823108d6f32ff7cd878ed250c77
Gabriel-Reis021 <gabrielreissantos180@gmail.com>
2025-07-24 14:48:35 -0300

    estilizando introdução e experiencia para o modo escuro

--- web/index.js
+++ web/index.js
@@ -1,6 +1,7 @@
 botao = document.querySelector('.botao-noturno');
 botao.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
+
     if (document.body.classList.contains('dark-mode')) {
         botao.innerHTML = '<img src="../img/sun.png" width="30" height="30" alt="">';
        
